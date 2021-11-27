750777ebaf83b8eda8d7951aeb662c8d4de2355d
DanteDeGrave <vladimir.system@gmail.com>
2021-11-27 14:58:37 +0500

    Создание моков и отрисовка элементов с данными

--- src/main.js
+++ src/main.js
@@ -3,17 +3,18 @@ import {createNavigationTemplate} from './view/site-navigation-view';
 import {createFiltersTemplate} from './view/site-filters-view';
 import {createSiteSortFormTemplate} from './view/site-sort-form-view';
 import {createEventsListTemplate} from './view/site-events-list';
-import {createAddPointFormTemplate} from './view/site-add-point-form-view';
 import {createEditPointFormTemplate} from './view/site-edit-point-form-view';
 import {createEventsItemTemplate} from './view/site-events-item';
+import {generateWaypoint} from './mock/waypoint';
 
-const WAYPOINT_COUNT = 3;
+const WAYPOINT_COUNT = 20;
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
   AFTEREND: 'afterend',
 };
+const points = Array.from({length: WAYPOINT_COUNT}, generateWaypoint);
 const tripMainElement = document.querySelector('.trip-main');
 const tripNavigationElement = tripMainElement.querySelector('.trip-controls__navigation');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
@@ -24,17 +25,18 @@ const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-renderTemplate(tripMainElement, createSiteInfoTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripMainElement, createSiteInfoTemplate(points), RenderPosition.AFTERBEGIN);
 renderTemplate(tripNavigationElement, createNavigationTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(tripFiltersElement, createFiltersTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(tripEventsElement, createSiteSortFormTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(tripEventsElement, createEventsListTemplate(), RenderPosition.BEFOREEND);
 
-const eventListElement = pageMainElement.querySelector('.trip-events__list');
+if (points.length) {
+  renderTemplate(tripEventsElement, createEventsListTemplate(), RenderPosition.BEFOREEND);
 
-renderTemplate(eventListElement, createEditPointFormTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(eventListElement, createAddPointFormTemplate(), RenderPosition.BEFOREEND);
+  const eventListElement = pageMainElement.querySelector('.trip-events__list');
 
-for (let i = 0; i < WAYPOINT_COUNT; i++) {
-  renderTemplate(eventListElement, createEventsItemTemplate(), RenderPosition.BEFOREEND);
+  renderTemplate(eventListElement, createEditPointFormTemplate(points[0]), RenderPosition.BEFOREEND);
+  for (let i = 1; i < points.length; i++) {
+    renderTemplate(eventListElement, createEventsItemTemplate(points[i]), RenderPosition.BEFOREEND);
+  }
 }
--- src/view/site-events-item.js
+++ src/view/site-events-item.js
@@ -1,32 +1,57 @@
-export const createEventsItemTemplate = () => (
-  `
+import dayjs from 'dayjs';
+
+export const createEventsItemTemplate = (point) => {
+  const {dueDate, price, wayPointType, timeStart, timeEnd, destination, offer, isFavorite} = point;
+  const favorite = isFavorite ? 'event__favorite-btn--active' : '';
+  const timeDifference = dayjs(timeEnd).diff(timeStart, 'second');
+  const elapsedTime = {
+    day: Math.round(timeDifference / 60 / 60 / 24),
+    hour: Math.round(timeDifference / 60 / 60 % 24),
+    minute: Math.round(timeDifference / 60 % 60),
+  };
+
+  const getElapsedTimeString = () => {
+    const day = elapsedTime.day < 10 ? `0${elapsedTime.day}D`: `${elapsedTime.day}D`;
+    const hour = elapsedTime.hour < 10 ? `0${elapsedTime.hour}H`: `${elapsedTime.hour}H`;
+    const minute = elapsedTime.minute < 10 ? `0${elapsedTime.minute}M`: `${elapsedTime.minute}M`;
+    if (elapsedTime.day) {
+      return `${day} ${hour} ${minute} `;
+    }
+    if (elapsedTime.hour) {
+      return `${hour} ${minute} `;
+    }
+    return `${minute} `;
+  };
+  const getOffersList = (offerData) => offerData.offers.map((element) => `
+    <li class="event__offer">
+      <span class="event__offer-title">${element.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${element.price}</span>
+    </li>
+  `).join('');
+  const offersList = getOffersList(offer);
+  return `
     <li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-18">MAR 18</time>
+        <time class="event__date" datetime="${dayjs(dueDate).format('YYYY-MM-D')}">${dayjs(dueDate).format('MMM D')}</time>
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${wayPointType.toLowerCase()}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">Taxi Amsterdam</h3>
+        <h3 class="event__title">${wayPointType} ${destination.name}</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            <time class="event__start-time" datetime="${dayjs(timeStart).format('YYYY-MM-DDTHH:mm')}">${dayjs(timeStart).format('HH:mm')}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+            <time class="event__end-time" datetime="${dayjs(timeEnd).format('YYYY-MM-DDTHH:mm')}">${dayjs(timeEnd).format('HH:mm')}</time>
           </p>
-          <p class="event__duration">30M</p>
+          <p class="event__duration">${getElapsedTimeString()}</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
-        <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">20</span>
-          </li>
-        </ul>
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        ${offer.offers.length ? `<ul class="event__selected-offers">${offersList}</ul>` : ''}
+        <button class="event__favorite-btn ${favorite}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -37,5 +62,5 @@ export const createEventsItemTemplate = () => (
         </button>
       </div>
     </li>
-  `
-);
+  `;
+};
